012d4f96ea3745f4d447fd359cd114b58c013c85
longbuivan <longbuivan95@gmail.com>
2022-12-04 17:43:43 +0700

    first commit

---
+++ server/routes/record.js
@@ -0,0 +1,108 @@
+const { application } = require("express");
+const bodyParser= require('body-parser')
+const express = require("express");
+
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /listings.
+const recordRoutes = express.Router();
+
+const dbo = require("../db/conn")
+
+recordRoutes.route("/").get(async function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+// This section will help you get a list of all the documents.
+recordRoutes.route("/listings").get(async function (req, res) {
+    const dbConnect = dbo.getDb();
+  
+    dbConnect
+      .collection("geoHourlyAggregation")
+      .find({}).limit(10)
+      .toArray(function (err, result) {
+        if (err) {
+          res.status(400).send("Error fetching listings!");
+       } else {
+          res.json(result);
+        }
+      });
+  });
+
+
+  // This section will help you get a single record by id
+recordRoutes.route("/listings/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect
+    .collection("records")
+    .findOne(myquery, function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+ });
+  
+
+  // This section will help you create a new document.
+recordRoutes.route("/listings/recordSwipe").post(function (req, res) {
+    const dbConnect = dbo.getDb();
+    const matchDocument = {
+      listing_id: req.body.id,
+      last_modified: new Date(),
+      session_id: req.body.session_id,
+      direction: req.body.direction
+    };
+  
+    dbConnect
+      .collection("matches")
+      .insertOne(matchDocument, function (err, result) {
+        if (err) {
+          res.status(400).send("Error inserting matches!");
+        } else {
+          console.log(`Added a new match with id ${result.insertedId}`);
+          res.status(204).send();
+        }
+      });
+  });
+
+
+  // This section will help you update a document by id.
+recordRoutes.route("/listings/updateLike").post(function (req, res) {
+  const dbConnect = dbo.getDb();
+  const listingQuery = { _id: req.body.id };
+  const updates = {
+    $inc: {
+      likes: 1
+    }
+  };
+
+  dbConnect
+    .collection("matches")
+    .updateOne(listingQuery, updates, function (err, _result) {
+      if (err) {
+        res.status(400).send(`Error updating likes on listing with id ${listingQuery.id}!`);
+      } else {
+        console.log("1 document updated");
+      }
+    });
+});
+
+
+// This section will help you delete a record.
+recordRoutes.route("/listings/delete/:id").delete((req, res) => {
+    const dbConnect = dbo.getDb();
+    const listingQuery = { listing_id: req.body.id };
+  
+    dbConnect
+      .collection("matches")
+      .deleteOne(listingQuery, function (err, _result) {
+        if (err) {
+          res.status(400).send(`Error deleting listing with id ${listingQuery.listing_id}!`);
+        } else {
+          console.log("1 document deleted");
+        }
+      });
+  });
+
+
+module.exports = recordRoutes;
